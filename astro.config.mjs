2a4aa5b8cfb89153862b31db3ef3085caf931210
thesethtruth <s.w.p.vanwieringen@gmail.com>
2026-01-29 16:10:21 +0100

    add shadcn

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,16 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
+import svelte from '@astrojs/svelte';
+
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: 'https://example.com',
+  integrations: [mdx(), sitemap(), svelte()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
